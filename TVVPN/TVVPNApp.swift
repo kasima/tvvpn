77dfb77891e9cc6200043cae35440ed6fe0acd24
Kasima Tharnpipitchai <kasima@gmail.com>
2023-02-07 21:52:15 +0700

    Initial Commit

---
+++ TVVPN/TVVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVVPNApp.swift
+//  TVVPN
+//
+//  Created by kasima on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TVVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
